c1afd698743b7be25b7536ed009db80ece5ed276
shashwata <shashwatasamanta5@gmail.com>
2024-06-13 23:12:35 +0530

    Added

--- 70BSTquestions/d.cpp
+++ 70BSTquestions/d.cpp
@@ -24,10 +24,90 @@ Sample output 1:
 
 Solution:
 Apporach 1:
-Here we do a inorder traversal of the entire tree
+Here we do a inorder traversal of the entire tree and store the output in an array, Now we wil traverse that 
+array to find the key val and print the successor and the predeccesor.
+ts: o(n)
+sp: o(n) as we are going to use an array
+Apporach 2:
+Steps:
+1. Find the key val in the tree.
+2. the pred will be the max val of the left subtree 
+3. and succ will be the min val of the right subtree.
 
+
+TS: O(n)
+SP: O(1)
  */
+
+
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d) {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildBSTtree(Node* root) {
+    int data;
+    cin >> data;
+    if (data == -1) {
+        return NULL;
+    }
+    root = new Node(data);
+    cout << "Enter left of " << data << endl;
+    root->left = buildBSTtree(root->left);
+    cout << "Enter right of " << data << endl;
+    root->right = buildBSTtree(root->right);
+    return root;
+}
+
+pair<int,int> findpredandsucc(Node* root, int k){
+    // find the val of key:
+    Node* temp = root;
+    int pred = -1 , succ = -1;
+    // store the pred and the succ val as the temp
+    while (temp->data != k)
+    {
+        if(temp->data > k){
+            pred = temp->data;
+            temp = temp->left;
+        }
+        else{
+            succ = temp->data;
+            temp = temp->right;
+        }
+    }
+    // after this temp have reached the key val
+
+    // follow the step 2 & 3
+    Node* lefttree = temp->left;
+    while (lefttree!=NULL)
+    {
+        pred = lefttree->data;
+        lefttree=lefttree->right;
+    }
+
+    Node* righttree = temp->right;
+    while (righttree!=NULL)
+    {
+        succ = righttree->data;
+        righttree = righttree->left;
+    }
+
+    return {pred, succ};
+}
+
+
 int main(){
+    Node * root = NULL;
+    root = buildBSTtree(root);
+    pair<int,int> ans = findpredandsucc(root,10);
+    cout<<ans.first<<" "<<ans.second<<endl;
 
 return 0;
 }
---
+++ 70BSTquestions/e.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include <bits/stdc++.h>
+#include <array>
+using namespace std;
+/* 
+ LCA of Two Nodes In A BST
+You are given a binary search tree of integers with N nodes. You are also given references to two nodes 
+'P' and 'Q' from this BST.
+Your task is to find the lowest common ancestor(LCA) of these two given nodes.
+The lowest common ancestor for two nodes P and Q is defined as the lowest node that has both P and Q as 
+descendants (where we allow a node to be a descendant of itself)
+
+eg: 
+Sample Input 1 :
+3 5
+2 1 3 -1 -1 -1 5 -1 -1
+Sample Output 1:
+3
+Solution:
+Apporach:
+we are gong to use recursion here 
+base case if the root == NULL return NULL
+if the root data is less than both p and q then move to the right subtree to find it 
+else move to the right subtree.
+TS: O(n)
+SP: o(1)
+
+Apporach 2:
+Using iterative method:
+TS: O(1)
+Sp O(1)
+*/
+
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int d) {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildBSTtree(Node* root) {
+    int data;
+    cin >> data;
+    if (data == -1) {
+        return NULL;
+    }
+    root = new Node(data);
+    cout << "Enter left of " << data << endl;
+    root->left = buildBSTtree(root->left);
+    cout << "Enter right of " << data << endl;
+    root->right = buildBSTtree(root->right);
+    return root;
+}
+
+Node* findLCA(Node* root , int p , int q){
+    if(root == NULL){
+        return NULL;
+    }
+    if(root->data > p && root->data > q){
+        return findLCA(root->left,p,q);
+    }
+    if(root->data < p && root->data < q){
+        return findLCA(root->right,p,q);
+    }
+    return root;
+}
+
+int main(){
+    Node* root = NULL;
+    root = buildBSTtree(root);
+    Node* ans = findLCA(root, 3,5);
+    cout<<ans->data;
+return 0;
+}
